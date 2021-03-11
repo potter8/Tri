8fe2ed1883f6924fbd2d491b72202af88915856c
potter8 <77454298+potter8@users.noreply.github.com>
2021-03-11 15:23:31 -0500

    Add files via upload

---
+++ Tri.py
@@ -0,0 +1,17 @@
+a, b, c = map(int, input().split())
+if a + b == c:
+    print(str(a) + "+" + str(b) + "=" + str(c))
+elif a - b == c:
+    print(str(a) + "-" + str(b) + "=" + str(c))
+elif a * b == c:
+    print(str(a) + "*" + str(b) + "=" + str(c))
+elif a / b == c:
+    print(str(a) + "/" + str(b) + "=" + str(c))
+elif a == b + c:
+    print(str(a) + "=" + str(b) + "+" + str(c))
+elif a == b - c:
+    print(str(a) + "=" + str(b) + "-" + str(c))
+elif a == b / c:
+    print(str(a) + "=" + str(b) + "/" + str(c))
+elif a == b * c:
+    print(str(a) + "=" + str(b) + "*" + str(c))
